e32310f70b9f2ceae7b82b6adff32546be234f27
Ynta <ybagdar@mail.ru>
2024-02-08 07:55:14 +0600

    deleted folder functions1

--- lab3/functions1/1.py
+++
@@ -1,8 +0,0 @@
-def grams_to_ounces(x):
-	return 28.3495231 * x
- 
-grams= float(input("enter values in grams: "))
-ounces = grams_to_ounces(grams)
-print(ounces)
-
-
--- lab3/functions1/2.py
+++
@@ -1,9 +0,0 @@
-
-def f_to_c(f):
-    return (5.0/9.0) * (f - 32)
-
-ff=float(input("Enter value in fahrenheit: "))
-c = f_to_c(ff)
-print("Value in celsius: ", c)
-
-
