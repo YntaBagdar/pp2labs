1aade3fd1abbc21260b2e6a2075ffd7501317617
Ynta <ybagdar@mail.ru>
2024-02-14 21:18:55 +0600

    added folder 2.12

---
+++ lab4/PythonDate.py
@@ -0,0 +1,56 @@
+
+#Write a Python program to subtract five days from current date.
+
+
+import datetime
+x = datetime.datetime.now().date()
+y = datetime.timedelta(5)
+
+print(x-y)
+
+
+
+#Write a Python program to print yesterday, today, tomorrow.
+import datetime
+x = datetime.datetime.now().date()
+y = datetime.timedelta(1)
+
+print("Today is: ", x)
+print("Yesterday was: ", x-y)
+print("Tomorrow will be: ", x+y)
+
+
+
+
+#Write a Python program to drop microseconds from datetime.
+import datetime
+x = datetime.datetime.now()
+print("Microseconds from datetime: ", x.strftime("%f"))
+
+
+
+
+#Write a Python program to calculate two date difference in seconds.
+"""import datetime
+x = datetime.datetime.now()
+y = datetime.timedelta(1)
+z = x-y
+print(z.strftime("%f"))"""
+
+from datetime import datetime,
+def date_diff_in_Seconds(dt2, dt1):
+  timedelta = dt2 - dt1
+  return timedelta.days * 24 * 3600 + timedelta.seconds
+#Specified date
+date1 = datetime.strptime('2015-01-01 01:00:00', '%Y-%m-%d %H:%M:%S')
+#Current date
+date2 = datetime.now()
+print("\n%d seconds" %(date_diff_in_Seconds(date2, date1)))
+print()
+
+
+
+
+
+
+
